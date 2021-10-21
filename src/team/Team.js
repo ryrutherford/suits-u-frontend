7737b25e86ccb2aa80c43ff15cd83f2058e19d85
Ry Rutherford <ry.rutherford99@gmail.com>
2021-10-20 21:50:07 -0400

    Update team page, minor style updates for mobile view

--- src/team/Team.js
+++ src/team/Team.js
@@ -3,118 +3,41 @@ import React from "react";
 const Team = () => {
     return (
         <section className="team">
-            <div className="team__title">
-                <h2 id="executive">Executive</h2>
-            </div>
             <div className="team__members">
                 <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1605830574/suitsu/IMG_5861_2_-min_1_mg33rn.jpg" alt="Ry Rutherford"/>
-                    <h3>Ry Rutherford</h3>
+                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609450066/suitsu/134933471_2788961834659220_1943239179967224248_n_kkysnp.jpg" alt="Drew Kersh"/>
+                    <h3>Drew Kersh</h3>
                     <p>Co-President</p>
-                    <p>U3 | BSc Computer Science</p>
                 </div>
                 <div className="team__members-member">
                     <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609361376/suitsu/zz_ih4qzo.jpg" alt="Samantha Sagredo"/>
                     <h3>Samantha Sagredo</h3>
                     <p>Co-President</p>
-                    <p>U2 | BCom Retail Management and Managing for Sustainability</p>
                 </div>
-            </div>
-            <div className="team__title">
-                <h2 id="bd">Business Development</h2>
-            </div>
-            <div className="team__members">
-                <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609450066/suitsu/134933471_2788961834659220_1943239179967224248_n_kkysnp.jpg" alt="Drew Kersh"/>
-                    <h3>Drew Kersh</h3>
-                    <p>VP Business Development</p>
-                    <p>U2 | BA Economics</p>
-                </div>
-                <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609362773/suitsu/134182136_513935382888245_4257848006120144097_n_rkhnrg.jpg" alt="Cole Millinger"/>
-                    <h3>Cole Millinger</h3>
-                    <p>Team Member Business Development</p>
-                    <p>U2 | BCom Business Analytics and Retail Management</p>
-                </div>
-                <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1610748954/suitsu/139721604_428515931679690_5237247749076098987_n_1_sd9hqo.jpg" alt="Gabby Urvari"/>
-                    <h3>Gabrielle Urvari</h3>
-                    <p>Team Member Business Development</p>
-                    <p>U2 | BA Philosophy</p>
-                </div>
-            </div>
-            <div className="team__title">
-                <h2 id="operations">Operations</h2>
-            </div>
-            <div className="team__members">
-                <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609450230/suitsu/134237260_1520307635024802_295039095221661968_n_befrep.jpg" alt="Emma Trudeau"/>
-                    <h3>Emma Trudeau</h3>
-                    <p>VP Operations</p>
-                    <p>U3 | BCom Finance</p>
-                </div>
-                <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1604027263/suitsu/123259673_812018182928562_848497563752829042_n_db3xkq.jpg" alt="Sarah Wang"/>
-                    <h3>Sarah Wang</h3>
-                    <p>Team Member Operations</p>
-                    <p>U2 | BCom International Management</p>
-                </div>
-                <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609361377/suitsu/134906142_775850183011459_4022504671340902922_n_eevfeh.jpg" alt="Arden Parker"/>
-                    <h3>Arden Parker</h3>
-                    <p>Team Member Operations</p>
-                    <p>U2 | BA&Sc Cognitive Science</p>
-                </div>
-            </div>
-            <div className="team__title">
-                <h2 id="marketing">Marketing</h2>
-            </div>
-            <div className="team__members">
                 <div className="team__members-member">
                     <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609450065/suitsu/133431882_3049786198454796_2213290369448932063_n_wh1ku6.jpg" alt="Arden McKillop"/>
                     <h3>Arden McKillop</h3>
                     <p>VP Marketing</p>
-                    <p>U2 | BCom Marketing</p>
                 </div>
                 <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1604027262/suitsu/123080240_477334346516391_9038941809618046832_n_i1wgbe.jpg" alt="Margaux Koene"/>
-                    <h3>Margaux Koeune</h3>
-                    <p>Team Member Marketing</p>
-                    <p>U2 | BA Industrial Relations</p>
+                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609361377/suitsu/134906142_775850183011459_4022504671340902922_n_eevfeh.jpg" alt="Arden Parker"/>
+                    <h3>Arden Parker</h3>
+                    <p>VP Operations</p>
                 </div>
                 <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609377784/suitsu/134092826_405915067350827_840273111619593022_n_g1mirc.jpg" alt="Grace Doris"/>
-                    <h3>Grace Doris</h3>
-                    <p>Team Member Marketing</p>
-                    <p>U2 | BA Sociology</p>
+                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1610748954/suitsu/139721604_428515931679690_5237247749076098987_n_1_sd9hqo.jpg" alt="Gabrielle Urvari"/>
+                    <h3>Gabrielle Urvari</h3>
+                    <p>VP Business Development</p>
                 </div>
-            </div>
-            <div className="team__title">
-                <h2 id="logistics">Logistics</h2>
-            </div>
-            <div className="team__members">
                 <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609130300/suitsu/133626822_4936611079747365_8259898813830558177_n_zhtrwd.jpg" alt="Ariane Pietri"/>
-                    <h3>Ariane Pietri</h3>
+                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1634760894/suitsu/242552607_570472337492491_8462258038107957750_n_ujxvwi.png" alt="Jamie Smith"/>
+                    <h3>Jamie Smith</h3>
                     <p>VP Logistics</p>
-                    <p>U2 | BCom Finance</p>
                 </div>
                 <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609130394/suitsu/133698197_220832249576892_5394423269881723020_n_pmqapw.jpg" alt="Jess Hanson"/>
-                    <h3>Jess Hanson</h3>
-                    <p>Team Member Logistics</p>
-                    <p>U2 | BSc Kinesiology</p>
-                </div>
-            </div>
-            <div className="team__title">
-                <h2 id="finance">Finance</h2>
-            </div>
-            <div className="team__members">
-                <div className="team__members-member">
-                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1609130473/suitsu/123138872_359803648560364_2071865284352020965_n_tc92cv.jpg" alt="Olivia Bosa"/>
+                    <img src="https://res.cloudinary.com/dpncams2q/image/upload/v1634780509/suitsu/243540434_1270905566705897_2107412772046460473_n_k1yetq.png" alt="Olivia Bosa"/>
                     <h3>Olivia Bosa</h3>
                     <p>VP Finance</p>
-                    <p>U2 | BCom Accounting and Finance</p>
                 </div>
             </div>
         </section>
